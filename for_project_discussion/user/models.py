ba4e5ffb298bc23b12805eeca9a95a2e6ea96004
Mahmoud Hefny <66621768+MahmoudHefny2001@users.noreply.github.com>
2022-12-26 20:42:29 +0200

    Delete for_project_discussion directory

--- for_project_discussion/user/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django import forms
-# Create your models here.
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-
-
